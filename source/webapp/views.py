2e58d3d9857899e87e75f80a5b765f0deb5364d7
jomagsm <jomagsm@gmail.com>
2020-08-01 10:46:14 +0600

    Добавил базовый шаблон, Главная страница и админка не подключенна

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(request):
+    is_admin = request.GET.get('is_admin', None)
+    if is_admin:
+        order_date = Product.objects.all()
+    else:
+        data = Product.objects.filter(amount__gt=1)
+        order_date = data.order_by('category', 'name')
+    return render(request, 'index.html', context={
+        'products': order_date,
+        'category': CATEGORY_CHOICE
+    })
