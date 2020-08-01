0de15e463f6b54921541fa3ac6abf1b1985cf41a
jomagsm <jomagsm@gmail.com>
2020-08-01 11:23:30 +0600

    Создал модльку. И сделал миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+DEFAUL_STATUS = 'active'
+STATUS_CHOICE = [(DEFAUL_STATUS, 'Активно'), ('blocked', 'Заблокированно')]
+
+
+class Note(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    email = models.EmailField(max_length=100, verbose_name='Электронная почта')
+    text = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Текст записи')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=20, verbose_name='Статус', choices=STATUS_CHOICE, default=DEFAUL_STATUS)
